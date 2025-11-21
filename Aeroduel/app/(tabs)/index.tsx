a5793cf231adae0010ce222f9aba4500cee47989
zack6yuan <zack6yuan@gmail.com>
2025-11-20 19:37:44 -0600

    added the spectate and news blocks.

--- Aeroduel/app/(tabs)/index.tsx
+++ Aeroduel/app/(tabs)/index.tsx
@@ -3,9 +3,17 @@ import * as SplashScreen from "expo-splash-screen";
 // import { router } from 'expo-router';
 import { LinearGradient } from "expo-linear-gradient";
 import { useEffect } from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
 import whitePlaneIcon from "../../assets/images/aeroduel-plane-white.png";
 import logout from "../../assets/images/log-out.png";
+import eye from "../../assets/images/eye.png";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -33,45 +41,84 @@ export default function HomeScreen() {
   // onPress={signOut}
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <LinearGradient
         colors={["#000000", "#FF0000"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         style={styles.backgroundGradient}
       ></LinearGradient>
-      <View style={styles.homePageHeader}>
-        <Text style={styles.headerText}>Welcome, [user]</Text>
-        <View style={styles.logoutContainer}>
-          <Text style={styles.logoutText}>Logout</Text>
-          <Image source={logout} style={styles.logoutIcon}></Image>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "flex-start",
+          position: "relative",
+        }}
+      >
+        <View style={styles.homePageHeader}>
+          <Text style={styles.headerText}>Welcome, [user]</Text>
+          <View style={styles.logoutContainer}>
+            <Text style={styles.logoutText}>Logout</Text>
+            <Image source={logout} style={styles.logoutIcon}></Image>
+          </View>
         </View>
-      </View>
-      <View>
-        <View style={styles.enterDuel}>
-          <View style={styles.topRow}>
-            <View style={styles.blockText}>
-              <Text style={styles.blockHeader}>Enter the Duel</Text>
-              <Text style={styles.blockSubtitle}>
-                Join with a QR code, or start a new match
-              </Text>
+        <View>
+          <View style={styles.enterDuel}>
+            <View style={styles.topRow}>
+              <View style={styles.blockText}>
+                <Text style={styles.blockHeader}>Enter the Duel</Text>
+                <Text style={styles.blockSubtitle}>
+                  Join with a QR code, or start a new match
+                </Text>
+              </View>
+              <Image
+                source={whitePlaneIcon}
+                style={styles.whitePlaneIcon}
+              ></Image>
             </View>
-            <Image
-              source={whitePlaneIcon}
-              style={styles.whitePlaneIcon}
-            ></Image>
+            <View style={styles.bottomRow}>
+              <Pressable style={styles.joinButton}>
+                <Text style={styles.buttonText}>Join Match</Text>
+              </Pressable>
+              <Pressable style={styles.startButton}>
+                <Text style={styles.buttonText}>Start Match</Text>
+              </Pressable>
+            </View>
           </View>
-          <View style={styles.blockButtons}>
-            <Pressable style={styles.joinButton}>
-              <Text style={styles.buttonText}>Join Match</Text>
-            </Pressable>
-            <Pressable style={styles.startButton}>
-              <Text style={styles.buttonText}>Start Match</Text>
-            </Pressable>
+          <View style={styles.enterDuel}>
+            <View style={styles.topRow}>
+              <View style={styles.blockText}>
+                <Text style={styles.blockHeader}>Spectate</Text>
+                <Text style={styles.blockSubtitle}>
+                  View the scoreboards of ongoing duels
+                </Text>
+              </View>
+              {/* <Image (Need to get a higher quality icon)
+              source={eye}
+              style={styles.eyeIcon}
+            ></Image> */}
+            </View>
+            <View style={styles.bottomRow}>
+              <Pressable style={styles.qrButton}>
+                <Text style={styles.buttonText}>Join with QR</Text>
+              </Pressable>
+              <Pressable style={styles.pinButton}>
+                <Text style={styles.buttonText}>Enter Pin</Text>
+              </Pressable>
+            </View>
+          </View>
+          <View style={styles.enterDuel}>
+            <View style={styles.topRow}>
+              <View style={styles.blockText}>
+                <Text style={styles.blockHeader}>Latest News</Text>
+                <Text style={styles.blockSubtitle}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sapien ligula, accumsan lectus tincidunt quis. Sed faucibus laoreet nibh, ac euismod metus molestie. Dolor sit amet, consectetur adipisc
+                </Text>
+              </View>
+            </View>
           </View>
         </View>
-        <View></View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -106,7 +153,14 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   topRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
+  },
+  bottomRow: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 7,
+    justifyContent: "center",
+    paddingBottom: 10,
   },
   blockText: {
     paddingTop: 10,
@@ -120,16 +174,10 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontWeight: "700",
   },
-  blockButtons: {
-    display: 'flex',
-    flexDirection: 'row',
-    gap: 7,
-    justifyContent: 'center',
-    paddingBottom: 10,
-  },
   logoutContainer: {
     display: "flex",
     flexDirection: "row",
+    marginLeft: 24,
   },
   logoutIcon: {
     width: 25,
@@ -143,6 +191,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
     marginTop: 5,
+    marginBottom: 5,
     paddingLeft: 20,
     paddingRight: 20,
     height: 220,
@@ -156,21 +205,37 @@ const styles = StyleSheet.create({
     width: 90,
     height: 90,
   },
+  eyeIcon: {
+    width: 100,
+    height: 100,
+  },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 20,
   },
   joinButton: {
-    backgroundColor: '#333333ff',
+    backgroundColor: "#333333ff",
     paddingHorizontal: 45,
     paddingVertical: 20,
     borderBottomLeftRadius: 15,
   },
   startButton: {
-    backgroundColor: '#333333ff',
+    backgroundColor: "#333333ff",
     paddingHorizontal: 45,
     paddingVertical: 20,
     borderBottomRightRadius: 15,
-  }
+  },
+  qrButton: {
+    backgroundColor: "#333333ff",
+    paddingHorizontal: 35,
+    paddingVertical: 20,
+    borderBottomLeftRadius: 15,
+  },
+  pinButton: {
+    backgroundColor: "#333333ff",
+    paddingHorizontal: 55,
+    paddingVertical: 20,
+    borderBottomRightRadius: 15,
+  },
 });
