4bb62c498c77bfd02f0dc7ca867e39f45d0a2a52
zack6yuan <zack6yuan@gmail.com>
2025-11-22 11:10:57 -0600

    content is on the settings page.

--- Aeroduel/components/SettingsHeader.tsx
+++ Aeroduel/components/SettingsHeader.tsx
@@ -1,10 +1,67 @@
-// import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import settingsGear from "../assets/images/settings-gear-white.png"
 
 export default function SettingsHeader() {
   return (
-    {/* Settings header implemented here */}
-  )
+    <View style={styles.settingsContainer}>
+      <View style={styles.settingsRow}>
+        <View>
+          <Text style={styles.settingsHeader}>Settings</Text>
+          <Text style={styles.settingsSubtitle}>
+            Control Your App Experience
+          </Text>
+        </View>
+        <View>
+          <Image source={settingsGear} style={styles.settingsGearIcon}></Image>
+        </View>
+      </View>
+    </View>
+  );
 }
 
-// const styles = StyleSheet.create({
-// })
+const styles = StyleSheet.create({
+  settingsContainer: {
+    backgroundColor: "rgba(145, 72, 72, 0.5)", // rgba to set opacity for background
+    opacity: 100,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    borderRadius: 15,
+    paddingTop: 35,
+    // Need to play with the shadow colors
+    // shadowColor: '#512929ff',
+    // shadowOffset: { width: 10, height: 10 },
+    // shadowOpacity: 10,
+    // shadowRadius: 5,
+  },
+  settingsRow: {
+    flexDirection: "row",
+    display: "flex",
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  settingsHeader: {
+    color: "#FFFFFF",
+    fontSize: 40,
+
+    fontFamily: "Coolvetica-Regular",
+  },
+  settingsSubtitle: {
+    color: "#FFFFFF",
+    fontWeight: "700",
+    paddingBottom: 20,
+  },
+  blockAuthor: {
+    paddingTop: 20,
+    fontSize: 17,
+  },
+  settingsGearIcon: {
+    width: 70,
+    height: 70,
+    justifyContent: 'flex-end',
+  }
+});
--- Aeroduel/components/SettingsOptions.tsx
+++ Aeroduel/components/SettingsOptions.tsx
@@ -1,10 +1,85 @@
-// import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 export default function SettingsOptions() {
+  // We might need to move every settings option to a seperate component for spacing and formatting.
   return (
-    {/* Settings options implemented here */}
-  )
+    <View style={styles.settingsContainer}>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>Username</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>Password</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>Clear Cache</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>FAQ</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>Help</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>About</Text>
+        </View>
+      </Pressable>
+      <Pressable style={styles.settingsOption}>
+        <View>
+          <Text style={styles.settingsHeader}>Delete Account</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
 
-// const styles = StyleSheet.create({
-// })
+const styles = StyleSheet.create({
+  settingsContainer: {
+    backgroundColor: "rgba(145, 72, 72, 0.5)", // rgba to set opacity for background
+    opacity: 100,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 15,
+    gap: 15,
+    // Need to play with the shadow colors
+    // shadowColor: '#512929ff',
+    // shadowOffset: { width: 10, height: 10 },
+    // shadowOpacity: 10,
+    // shadowRadius: 5,
+  },
+  settingsOption: {
+    flexDirection: "row",
+    display: "flex",
+  },
+  settingsHeader: {
+    color: "#FFFFFF",
+    fontSize: 40,
+
+    fontFamily: "Coolvetica-Regular",
+  },
+  settingsSubtitle: {
+    color: "#FFFFFF",
+    fontWeight: "700",
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  blockAuthor: {
+    paddingTop: 20,
+    fontSize: 17,
+  },
+});
